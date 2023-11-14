88ef149537dc82c7347be92f663fb800673493f2
manuhegde1309 <manuhegde1309@gmail.com>
2023-11-14 10:17:54 +0530

    added files

--- frontend/src/components/studentportal/studentPortalRegister.jsx
+++ frontend/src/components/studentportal/studentPortalRegister.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import {React,useState} from 'react'
 import { toast } from 'react-hot-toast'
-import {useNavigate} from 'react-router-dom' 
+import {Link, useNavigate} from 'react-router-dom' 
 import './studentportal.css'
 import Back from '../common/back/Back'
+
 const StudentPortalRegister = () => {
     const navigate = useNavigate()
     const [data,setData]=useState({
@@ -25,31 +26,35 @@ const StudentPortalRegister = () => {
             } else {
                 setData({})
                 toast.success('Login Successfull.Welcome!')
-                navigate('/login')
+                navigate('/dashboard')
             }
         } catch (error) {
             console.log(error)
         }
 
     }
+  
   return (
     <>
     <Back/>
+    <div>
     <form onSubmit={registerUser} className='form'>
         <div>
-        <label className='field'>Name</label>
+        <label className='field'>Name:</label>
         <input type='text' placeholder='enter name' value={data.name} onChange={(e)=>setData({...data,name:e.target.value})}/>
         </div>
         <div>
-        <label className='field'>Email</label>
+        <label className='field'>Email:</label>
         <input type='email' placeholder='enter email' value={data.email} onChange={(e)=>setData({...data,email:e.target.value})}/>
         </div>
         <div>
-        <label className='field'>password</label>
+        <label className='field'>password:</label>
         <input type='password' placeholder='enter password' value={data.password} onChange={(e)=>setData({...data,password:e.target.value})}/>
         </div>
         <button type='submit'>Submit</button>
+        <Link to='/login'><button type='button'>Login</button></Link>
     </form>
+    </div>
     </>
   )
 }
